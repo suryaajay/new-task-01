f3680ad70765184dd30ee4a6ab36d6d3d4346b18
suryaajay <92297761+suryaajay@users.noreply.github.com>
2025-02-23 11:27:02 +0530

    Add files via upload

---
+++ Main5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int reversedNum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversedNum);
+        scanner.close();
+    }
+}
